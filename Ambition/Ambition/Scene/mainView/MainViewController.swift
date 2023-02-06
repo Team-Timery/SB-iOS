6756402f5329e50801cbbba306c419e0feb5a018
Byung jin <bjcho1503@naver.com>
2023-02-06 23:15:38 +0900

    ⚙️ :: 프로젝트 설정

---
+++ Ambition/Ambition/Scene/mainView/MainViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MainViewController.swift
+//  Ambition
+//
+//  Created by 조병진 on 2023/02/06.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class MainViewController: UIViewController {
+    
+    private let label = UILabel().then {
+        $0.text = "MainView"
+        $0.textColor = .black
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+    override func viewDidLayoutSubviews() {
+        addSubViews()
+        makeConstraints()
+    }
+}
+
+extension MainViewController {
+    private func addSubViews() {
+        view.addSubview(label)
+    }
+    
+    private func makeConstraints() {
+        label.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+}
