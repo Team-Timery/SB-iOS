04f2b6783aae23c09102158bd06701f7e4b13acf
Byung jin <bjcho1503@naver.com>
2023-02-09 20:53:03 +0900

    📝 :: AuthNextButton Component

---
+++ Ambition/Ambition/Souce/Component/Auth/AuthNextButton.swift
@@ -0,0 +1,15 @@
+//
+//  AuthNextButton.swift
+//  Ambition
+//
+//  Created by 조병진 on 2023/02/09.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+import Then
+
+class AuthNextButton: UIButton {
+    
+}
