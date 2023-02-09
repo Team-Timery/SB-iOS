2d050eff74a7ea6de558f3bedbe8dc1b9b0996a5
Byung jin <bjcho1503@naver.com>
2023-02-09 20:51:47 +0900

    📝 :: Extension 생성

---
+++ Ambition/Ambition/Souce/Extension/UIColor+Colors.swift
@@ -0,0 +1,26 @@
+//
+//  UIColor+Colors.swift
+//  Ambition
+//
+//  Created by 조병진 on 2023/02/09.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    //main
+    static let main = UIColor(named: "main")
+    static let mainDarken = UIColor(named: "main-Darken")
+    static let mainElevated = UIColor(named: "main-Elevated")
+    
+    //white
+    static let whiteElevated1 = UIColor(named: "white-Elevated1")
+    static let whiteElevated2 = UIColor(named: "white-Elevated2")
+    static let whiteElevated3 = UIColor(named: "white-Elevated3")
+    static let whiteElevated4 = UIColor(named: "white-Elevated4")
+    static let whiteElevated5 = UIColor(named: "white-Elevated5")
+    
+    //red
+    static let error = UIColor(named: "error")
+}
