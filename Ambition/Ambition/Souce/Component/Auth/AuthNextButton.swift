43c3f384f084f9cfaf8cfe84781593dc039eef81
Byung jin <bjcho1503@naver.com>
2023-02-10 21:42:47 +0900

    📝 :: AuthNextButton

--- Ambition/Ambition/Souce/Component/Auth/AuthNextButton.swift
+++ Ambition/Ambition/Souce/Component/Auth/AuthNextButton.swift
@@ -12,4 +12,16 @@ import Then
 
 class AuthNextButton: UIButton {
     
+    init(label: String) {
+        super.init(frame: .zero)
+        backgroundColor = .mainElevated
+        setTitle(label, for: .normal)
+        setTitleColor(.white, for: .normal)
+        titleLabel?.font = UIFont.title2Bold
+        layer.cornerRadius = 30
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
