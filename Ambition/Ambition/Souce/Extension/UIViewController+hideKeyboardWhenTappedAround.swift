4846fc3fcfd3da89029ae8396d3e77bb0ed6665e
Byung jin <bjcho1503@naver.com>
2023-02-15 23:38:16 +0900

    📝 :: extension 추가

---
+++ Ambition/Ambition/Souce/Extension/UIViewController+hideKeyboardWhenTappedAround.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+hideKeyboardWhenTappedAround.swift
+//  Ambition
+//
+//  Created by 조병진 on 2023/02/13.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
