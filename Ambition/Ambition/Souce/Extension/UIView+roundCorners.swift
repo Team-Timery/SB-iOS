c9b712fa8984e17e1a8bbb50880084d2210cb3d4
Byung jin <bjcho1503@naver.com>
2023-02-20 18:45:14 +0900

    📝 :: extension 추가

---
+++ Ambition/Ambition/Souce/Extension/UIView+roundCorners.swift
@@ -0,0 +1,19 @@
+//
+//  UIView+roundCorners.swift
+//  Ambition
+//
+//  Created by 조병진 on 2023/02/20.
+//
+
+import Foundation
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func roundCorners(cornerRadius: CGFloat, maskedCorners: CACornerMask) {
+        clipsToBounds = false
+        layer.cornerRadius = cornerRadius
+        layer.maskedCorners = CACornerMask(arrayLiteral: maskedCorners)
+    }
+}
