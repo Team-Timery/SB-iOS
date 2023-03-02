fae324a29861caeb294dfe1e43b02359e384bf60
Byung jin <bjcho1503@naver.com>
2023-03-02 23:05:05 +0900

    📝 :: subjectModel 생성

---
+++ Ambition/Ambition/Souce/Scene/Analyze/Model/subjectTimeModel.swift
@@ -0,0 +1,15 @@
+//
+//  subjectTimeModel.swift
+//  Ambition
+//
+//  Created by 조병진 on 2023/03/02.
+//
+
+import Foundation
+
+struct subjectTimeModel {
+    let emoji: String
+    let name: String
+    let percent: String
+    let time: String
+}
