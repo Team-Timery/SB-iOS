ba07459c316834fdead18bfde830533bd589903b
baegteun <baegteun@gmail.com>
2023-06-04 21:28:29 +0900

    :sparkles: :: BaseViewController 세팅

--- Timery/Timery/Source/Base/BaseViewController.swift
+++ Timery/Timery/Source/Base/BaseViewController.swift
@@ -1,8 +1,26 @@
-//
-//  BaseViewController.swift
-//  Timery
-//
-//  Created by 최형우 on 2023/06/04.
-//
+import RxSwift
+import UIKit
 
-import Foundation
+open class BaseViewController<ViewModel: ViewModelType>:
+    UIViewController,
+    HasViewModel,
+    AddViewable,
+    ConstraintMakable,
+    HasDisposeBag,
+    ViewModelBindable {
+    public var viewModel: ViewModel
+    public var disposeBag: DisposeBag = DisposeBag()
+
+    public init(viewModel: ViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    open func addSubViews() {}
+    open func makeConstraints() {}
+    open func bind() {}
+}
